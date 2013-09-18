8ea0045b680da40ce020e54e1840271d6d8619b6
stephanos <github@stephanbehnke.de>
2013-09-18 13:19:39 +0200

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+reflector [![Build Status](https://secure.travis-ci.org/101loops/reflector.png)](https://travis-ci.org/stephanos/reflector)
+=========
+
+This Go package extends the standard package *reflect* with useful utilities:
+- type Multi []error
+- recovering from panic
+
+Install it: `go get github.com/101loops/errors`
+
+Documentation is available on [godoc.org](http://godoc.org/github.com/101loops/errors).
+
+License: MIT (see LICENSE).
---
+++ num.go
@@ -0,0 +1,85 @@
+package reflector
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func Float2Number(source float64, target interface{}) (err error) {
+	switch target := target.(type) {
+	case *int:
+		*target = int(source)
+	case *int8:
+		*target = int8(source)
+	case *int16:
+		*target = int16(source)
+	case *int32:
+		*target = int32(source)
+	case *int64:
+		*target = int64(source)
+	case *uint:
+		*target = uint(source)
+	case *uint8:
+		*target = uint8(source)
+	case *uint16:
+		*target = uint16(source)
+	case *uint32:
+		*target = uint32(source)
+	case *uint64:
+		*target = uint64(source)
+	case *float32:
+		*target = float32(source)
+	case *float64:
+		*target = source
+	default:
+		err = fmt.Errorf("target (%T) is not a number", target)
+	}
+	return
+}
+
+func Number2Float(source interface{}) (target float64, err error) {
+	switch num := source.(type) {
+	case int:
+		target = float64(num)
+	case int8:
+		target = float64(num)
+	case int16:
+		target = float64(num)
+	case int32:
+		target = float64(num)
+	case int64:
+		target = float64(num)
+	case uint:
+		target = float64(num)
+	case uint8:
+		target = float64(num)
+	case uint16:
+		target = float64(num)
+	case uint32:
+		target = float64(num)
+	case uint64:
+		target = float64(num)
+	case float32:
+		target = float64(num)
+	case float64:
+		target = num
+	default:
+		err = fmt.Errorf("source (%T) is not a number", source)
+	}
+	return
+}
+
+func IsNumber(obj interface{}) bool {
+	return isNumberKind(reflect.TypeOf(obj).Kind())
+}
+
+// HELPERS ========================================================================================
+
+func isNumberKind(kind reflect.Kind) (b bool) {
+	switch kind {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint,
+		reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Float32, reflect.Float64:
+		b = true
+	}
+	return
+}
