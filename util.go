863039391190a43fc8de7f02601a9e72c1a8956d
stephanos <github@stephanbehnke.de>
2014-06-22 13:42:59 +0200

    moved 'IsDefault' to separate package 'https://github.com/101loops/iszero'

--- util.go
+++ util.go
@@ -1,9 +1,6 @@
 package reflector
 
-import (
-	"reflect"
-	"time"
-)
+import "reflect"
 
 func IsExportableField(field reflect.StructField) bool {
 	return field.PkgPath == "" // PkgPath is empty for exported fields.
@@ -13,52 +10,12 @@ func IsStruct(obj interface{}) bool {
 	return isStructType(reflect.TypeOf(obj))
 }
 
-func IsPointer(obj interface{}) bool {
-	return isPointerType(reflect.TypeOf(obj))
-}
-
-func IsDefault(obj interface{}) (b bool) {
-	switch v := obj.(type) {
-	case bool:
-		b = (v == false)
-	case string:
-		b = (v == "")
-	case []byte:
-		b = len(v) == 0
-	case int:
-		b = (v == int(0))
-	case int8:
-		b = (v == int8(0))
-	case int16:
-		b = (v == int16(0))
-	case int32:
-		b = (v == int32(0))
-	case int64:
-		b = (v == int64(0))
-	case uint:
-		b = (v == uint(0))
-	case uint8:
-		b = (v == uint8(0))
-	case uint16:
-		b = (v == uint16(0))
-	case uint32:
-		b = (v == uint32(0))
-	case uint64:
-		b = (v == uint64(0))
-	case float32:
-		b = (v == float32(0))
-	case float64:
-		b = (v == float64(0))
-	case time.Time:
-		b = v.IsZero()
-	default:
-		// TODO: use reflection
-	}
-	return
-}
-
 func isStructType(typ reflect.Type) bool {
 	return typ.Kind() == reflect.Struct
+}
+
+func IsPointer(obj interface{}) bool {
+	return isPointerType(reflect.TypeOf(obj))
 }
 
 func isPointerType(typ reflect.Type) bool {
--- util_test.go
+++ util_test.go
@@ -1,41 +1,8 @@
 package reflector
 
-import (
-	. "github.com/101loops/bdd"
-	"time"
-)
-
-var (
-	defaultTime time.Time
-)
-
-type MyString string
+import . "github.com/101loops/bdd"
 
 var _ = Describe("Utility", func() {
-	It("is default", func() {
-		Check(IsDefault(true), Equals, false)
-		Check(IsDefault(false), Equals, true)
-
-		Check(IsDefault(5), Equals, false)
-		Check(IsDefault(0), Equals, true)
-		Check(IsDefault(int32(0)), Equals, true)
-		Check(IsDefault(int64(0)), Equals, true)
-
-		Check(IsDefault([]byte("ABC")), Equals, false)
-		Check(IsDefault([]byte("")), Equals, true)
-
-		Check(IsDefault("test"), Equals, false)
-		Check(IsDefault(""), Equals, true)
-		//Check(IsDefault(MyString("")), Equals, true) TODO?
-
-		Check(IsDefault(1.0), Equals, false)
-		Check(IsDefault(0.0), Equals, true)
-		Check(IsDefault(float32(0.0)), Equals, true)
-		Check(IsDefault(float64(0.0)), Equals, true)
-
-		Check(IsDefault(time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)), Equals, false)
-		Check(IsDefault(defaultTime), Equals, true)
-	})
 
 	It("is pointer", func() {
 		i := 5
